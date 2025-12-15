6149e40bd50b870fffbc4ef0feb10f25e5d9f3c1
mahadi40011 <mahadi40011@gmail.com>
2025-12-15 10:22:11 +0600

    Create AllTicket, Dashboard, Home page but no Content and set them into router

---
+++ src/pages/AllTicket/AllTicket.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllTicket = () => {
+  return (
+    <div>
+      All Ticket
+    </div>
+  );
+};
+
+export default AllTicket;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,11 +2,23 @@ import { createBrowserRouter } from "react-router";
 import MainLayout from "../layouts/MainLayout";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import Home from "../pages/Home/Home";
+import AllTicket from "../pages/AllTicket/AllTicket";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/all-ticket',
+        element: <AllTicket/>
+      },
+    ]
   },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <SignUp /> },
