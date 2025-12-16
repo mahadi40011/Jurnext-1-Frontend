0e2ccbf4787260afde42b93495680eddc9a247cf
mahadi40011 <mahadi40011@gmail.com>
2025-12-17 02:24:02 +0600

    Card design some change

--- src/components/Shared/Cards/AllTicketPageCard.jsx
+++ src/components/Shared/Cards/AllTicketPageCard.jsx
@@ -10,29 +10,37 @@ const AllTicketPageCard = ({ ticket }) => {
       <img src={image} alt={title} className="w-full h-40 object-cover" />
 
       <div className="p-4 flex flex-col gap-2">
-        <h3 className="text-lg font-semibold">{title}</h3>
-        <p className="text-sm text-gray-600">
-          {ticket.from} → {ticket.to}
-        </p>
-        <p className="text-sm">Transport Type: {transport}</p>
-        <p className="text-sm">
-          Departure: {date} {","} {time}
-        </p>
-        <p className="text-sm">Quantity: {quantity}</p>
-        <p className="text-sm font-medium">Total: ${price}</p>
-        <div className="lg:col-span-2 flex items-center gap-2">
-          <label className="label ">Perks: </label>
-          <div className="flex flex-wrap items-center gap-4">
-            {perks.map((perk) => (
-              <span key={perk} className="bg-green-500 px-2 rounded-lg text-sm">
-                {perk}
-              </span>
-            ))}
+        <div className="space-y-1">
+          <h3 className="text-lg font-semibold">{title}</h3>
+          <p className="text-sm text-gray-600">
+            {ticket.from} → {ticket.to}
+          </p>
+          <p className="text-sm">Transport Type: {transport}</p>
+          <p className="text-sm">
+            Departure: {date} {","} {time}
+          </p>
+          <p className="text-sm">Quantity: {quantity}</p>
+          <p className="text-sm font-medium">Total: ${price}</p>
+          <div className="lg:col-span-2 flex gap-2">
+            <label className="label ">Perks: </label>
+            <div className="flex flex-wrap items-center gap-2">
+              {perks.map((perk) => (
+                <span
+                  key={perk}
+                  className="bg-green-200 px-2 rounded-lg text-xs"
+                >
+                  {perk}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
 
         {/* See Details Button */}
-        <Link to={`/tickets/${_id}`} className="mt-2 w-full bg-lime-500 hover:bg-lime-600 text-white text-center py-2 rounded-lg font-semibold transition">
+        <Link
+          to={`/tickets/${_id}`}
+          className="mt-2 w-full bg-lime-500 hover:bg-lime-600 text-white text-center py-2 rounded-lg font-semibold transition"
+        >
           See Details
         </Link>
       </div>
