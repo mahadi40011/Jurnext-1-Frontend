ad242d38835e0383fb966dc968774d6cede0120d
mahadi40011 <mahadi40011@gmail.com>
2025-12-18 04:10:11 +0600

    get all user data from database by API fetching

--- src/pages/Dashboard/Admin/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/ManageUsers.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import ManageUsersTableRow from "../../../components/Shared/TableRows/ManageUsersTableRow";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
 
 const ManageUsers = () => {
+   const { user, loading } = useAuth();
+   const axiosSecure = useAxiosSecure();
+
+   const {
+     data: users = [],
+     isLoading,
+     refetch,
+   } = useQuery({
+     queryKey: ["users", user?.email],
+     queryFn: async () => {
+       const { data } = await axiosSecure(`/users`);
+       return data;
+     },
+   });
+
+  if (isLoading & loading) return <LoadingSpinner />;
+
   return (
     <div className="container mx-auto px-2 sm:px-4">
       <div className="overflow-x-auto  rounded-xl border border-gray-300">
