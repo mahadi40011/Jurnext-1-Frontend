96232c6b72b9ec76a8db0b2e8ecdaa56bf7835f7
mahadi40011 <mahadi40011@gmail.com>
2025-12-19 01:51:27 +0600

    1 bug fix

--- src/components/Shared/TableRows/RequestedBookingTableRow.jsx
+++ src/components/Shared/TableRows/RequestedBookingTableRow.jsx
@@ -25,7 +25,7 @@ const RequestedBookingTableRow = ({refetch, bookingReqData }) => {
   return (
     <tr>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p className="text-gray-900 ">{customer.email}</p>
+        <p className="text-gray-900 ">{customer?.email}</p>
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         <p className="text-gray-900 ">{ticketTitle}</p>
