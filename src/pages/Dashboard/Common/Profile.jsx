4fe94a6da33cdfc5e4e7de2c91062efdd6334649
mahadi40011 <mahadi40011@gmail.com>
2025-12-19 22:15:06 +0600

    Set user role in the profile page using custom hook

--- src/pages/Dashboard/Common/Profile.jsx
+++ src/pages/Dashboard/Common/Profile.jsx
@@ -1,52 +1,53 @@
-import useAuth from '../../../hooks/useAuth'
-import coverImg from '../../../assets/cover.jpg'
-// import useRole from '../../../hooks/useRole'
+import useAuth from "../../../hooks/useAuth";
+import coverImg from "../../../assets/cover.jpg";
+import useRole from "../../../hooks/useRole";
+import { PulseLoader } from "react-spinners";
 
 const Profile = () => {
-  const { user } = useAuth()
-  // const {role, isRoleLoading}= useRole()
-// console.log(role, isRoleLoading)
+  const { user } = useAuth();
+  const { role, isRoleLoading } = useRole();
+
   return (
-    <div className='flex justify-center items-center h-[92vh]'>
-      <div className='bg-white shadow-lg rounded-2xl '>
+    <div className="flex justify-center items-center h-[92vh]">
+      <div className="bg-white shadow-lg rounded-2xl ">
         <img
-          alt='cover photo'
+          alt="cover photo"
           src={coverImg}
-          className='w-full mb-4 rounded-t-lg h-56'
+          className="w-full mb-4 rounded-t-lg h-56"
         />
-        <div className='flex flex-col items-center justify-center p-4 -mt-16'>
-          <a href='#' className='relative block'>
+        <div className="flex flex-col items-center justify-center p-4 -mt-16">
+          <a href="#" className="relative block">
             <img
-              alt='profile'
+              alt="profile"
               src={user?.photoURL}
-              className='mx-auto object-cover rounded-full h-24 w-24  border-2 border-white '
+              className="mx-auto object-cover rounded-full h-24 w-24  border-2 border-white "
             />
           </a>
 
-          <p className='p-2 px-4 text-xs text-white bg-lime-500 rounded-full'>
-            {/* {role} */}
+          <p className="p-1 px-4 text-xs text-white bg-lime-500 rounded-full">
+            {isRoleLoading ? <PulseLoader size={7} /> : role}
           </p>
-          <p className='mt-2 text-xl font-medium text-gray-800 '>
+          <p className="mt-2 text-xl font-medium text-gray-800 ">
             User Id: {user?.uid}
           </p>
-          <div className='w-full p-2 mt-4 rounded-lg'>
-            <div className='flex flex-wrap items-center justify-between text-sm text-gray-600 '>
-              <p className='flex flex-col'>
+          <div className="w-full p-2 mt-4 rounded-lg">
+            <div className="flex flex-wrap items-center justify-between text-sm text-gray-600 ">
+              <p className="flex flex-col">
                 Name
-                <span className='font-bold text-gray-600 '>
+                <span className="font-bold text-gray-600 ">
                   {user?.displayName}
                 </span>
               </p>
-              <p className='flex flex-col'>
+              <p className="flex flex-col">
                 Email
-                <span className='font-bold text-gray-600 '>{user?.email}</span>
+                <span className="font-bold text-gray-600 ">{user?.email}</span>
               </p>
 
               <div>
-                <button className='bg-lime-500  px-10 py-1 rounded-lg text-white cursor-pointer hover:bg-lime-800 block mb-1'>
+                <button className="bg-lime-500  px-10 py-1 rounded-lg text-white cursor-pointer hover:bg-lime-800 block mb-1">
                   Update Profile
                 </button>
-                <button className='bg-lime-500 px-7 py-1 rounded-lg text-white cursor-pointer hover:bg-lime-800'>
+                <button className="bg-lime-500 px-7 py-1 rounded-lg text-white cursor-pointer hover:bg-lime-800">
                   Change Password
                 </button>
               </div>
@@ -55,7 +56,7 @@ const Profile = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
