cec70fbaa1dc2a5b5f575fa06823b48aa0161df0
mahadi40011 <mahadi40011@gmail.com>
2025-12-19 15:47:06 +0600

    fetch API for get all approved tickets in Advertise tickets page

--- src/pages/Dashboard/Admin/AdvertiseTickets.jsx
+++ src/pages/Dashboard/Admin/AdvertiseTickets.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import LoadingSpinner from '../../../components/Shared/LoadingSpinner';
 
 const AdvertiseTickets = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const { data: tickets = [], isLoading } = useQuery({
+    queryKey: "approved-tickets",
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/approved-tickets`);
+      return data;
+    },
+  });
+
+  if (isLoading) return <LoadingSpinner />;
+  console.log(tickets);
    
   return (
     <div>
