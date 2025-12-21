433757e02475c938cb29f5c4c48eb69fd196ee80
mahadi40011 <mahadi40011@gmail.com>
2025-12-22 01:55:37 +0600

    Create Popular Routes Card and set it the Popular routes Component

--- src/components/Home/PopularRoutes.jsx
+++ src/components/Home/PopularRoutes.jsx
@@ -1,3 +1,5 @@
+import PopularRoutesCard from "../Shared/Cards/PopularRoutesCard";
+
 const PopularRoutes = () => {
   const routes = [
     {
@@ -5,28 +7,29 @@ const PopularRoutes = () => {
       from: "Dhaka",
       to: "Cox's Bazar",
       price: "1200",
-      image: "https://images.unsplash.com/photo-1589985270826-4b7bb135bc9d",
+      image:
+        "https://i.ibb.co.com/qYsd3YSJ/images-q-tbn-ANd9-Gc-T2fu-F7-Bb-XNcic-Cboq-WRa-HLkhe9-It-Q-kk0-VQ-s.jpg",
     },
     {
       id: 2,
       from: "Sylhet",
       to: "Chittagong",
       price: "1500",
-      image: "https://images.unsplash.com/photo-1626014303757-6417749774ec",
+      image: "https://i.ibb.co.com/PGpL30c7/Chittagong.jpg",
     },
     {
       id: 3,
       from: "Dhaka",
       to: "Sylhet",
       price: "800",
-      image: "https://images.unsplash.com/photo-1590603740183-980e7f6920eb",
+      image: "https://i.ibb.co.com/PZ56bmWt/Sylhet.jpg",
     },
     {
       id: 4,
       from: "Rajshahi",
       to: "Dhaka",
       price: "1000",
-      image: "https://images.unsplash.com/photo-1622547748225-3fc4abd2cca0",
+      image: "https://i.ibb.co.com/9mNYFVzr/Dhaka.jpg",
     },
   ];
 
@@ -42,26 +45,7 @@ const PopularRoutes = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {routes.map((route) => (
-            <div
-              key={route.id}
-              className="relative group overflow-hidden rounded-2xl h-64 cursor-pointer"
-            >
-              <img
-                src={route.image}
-                className="absolute inset-0 w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
-                alt={route.to}
-              />
-              <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent"></div>
-              <div className="absolute bottom-4 left-4 text-white">
-                <p className="text-xs font-medium text-lime-400 uppercase tracking-widest">
-                  {route.from} to
-                </p>
-                <h3 className="text-xl font-bold">{route.to}</h3>
-                <p className="mt-2 text-sm font-semibold bg-white/20 backdrop-blur-md inline-block px-2 py-1 rounded">
-                  Starting from ${route.price}
-                </p>
-              </div>
-            </div>
+            <PopularRoutesCard route={route} />
           ))}
         </div>
       </div>
---
+++ src/components/Shared/Cards/PopularRoutesCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const PopularRoutesCard = ({ route }) => {
+  return (
+    <div
+      key={route.id}
+      className="relative group overflow-hidden rounded-2xl h-64 cursor-pointer"
+    >
+      <img
+        src={route.image}
+        className="absolute inset-0 w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+        alt={route.to}
+      />
+      <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent"></div>
+      <div className="absolute bottom-4 left-4 text-white">
+        <p className="text-xs font-medium text-lime-400 uppercase tracking-widest">
+          {route.from} to
+        </p>
+        <h3 className="text-xl font-bold">{route.to}</h3>
+        <p className="mt-2 text-sm font-semibold bg-white/20 backdrop-blur-md inline-block px-2 py-1 rounded">
+          Starting from ${route.price}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PopularRoutesCard;
